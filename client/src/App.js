8a605cd99e25d15fe1ece6ac579496a2487244db
iradbouzidi <irad.bouzidi@espit.com>
2022-10-15 14:47:20 +0100

    prepared boiler plate code

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Typography, AppBar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+import VideoPlayer from "./components/VideoPlayer";
+import Notifications from "./components/Notifications";
+import Options from "./components/Options";
+
+const App = () => {
+  return (
+    <div>
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Video Chat
+        </Typography>
+      </AppBar>
+      <VideoPlayer />
+      <Options>
+        <Notifications />
+      </Options>
+    </div>
+  );
+};
+
+export default App;
